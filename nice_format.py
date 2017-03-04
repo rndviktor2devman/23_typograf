64c5b479a954e74227f81743a0d145bc7d3dcc3d
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-03-05 00:59:08 +0300

    some formats

--- nice_format.py
+++ nice_format.py
@@ -15,6 +15,13 @@ patterns = [
     (r'\t', r' '),
     # exceed unix rows
     (r'\n{2,}', r'\n'),
+    # phone numbers
+    (r'\b[\+\(]?(\d)[-\s+\(]?(\d{2,3})[-\s+\)]?\s?'
+        r'(\d{2,3})[-\s]?(\d{1,3})[-\s]?(\d+)\b', r'\1(\2)\3–\4–\5'),
+    # numbers followed by words
+    (r'[^0-9]([а-яА-Яa-zA-Z.]+)\s?(\d+)', r' \1{}\2'.format(char_format)),
+    # words followed by numbers
+    (r'(\d+[.,]?\d+)\s?([€$\w]+)', r'\1{}\2'.format(char_format)),
 ]
 
 
