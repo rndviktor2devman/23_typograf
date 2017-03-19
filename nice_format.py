37c2585b36dbf5121d91f44979ff169e66e12ee3
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-03-19 13:41:24 +0300

    UPPER_CASE

--- nice_format.py
+++ nice_format.py
@@ -1,8 +1,8 @@
 import re
 
-char_format = '\u00A0'
+CHAR_FORMAT = '\u00A0'
 
-patterns = [
+PATTERNS = [
     # quotes
     (r'([\'"])(.*?)(\1)', r'«\2»'),
     # hyphen to dash
@@ -19,13 +19,13 @@ patterns = [
     (r'\b[\+\(]?(\d)[-\s+\(]?(\d{2,3})[-\s+\)]?\s?'
         r'(\d{2,3})[-\s]?(\d{1,3})[-\s]?(\d+)\b', r'\1(\2)\3–\4–\5'),
     # numbers followed by words
-    (r'[^0-9]([а-яА-Яa-zA-Z.]+)\s?(\d+)', r' \1{}\2'.format(char_format)),
+    (r'[^0-9]([а-яА-Яa-zA-Z.]+)\s?(\d+)', r' \1{}\2'.format(CHAR_FORMAT)),
     # words followed by numbers
-    (r'(\d+[.,]?\d+)\s?([€$\w]+)', r'\1{}\2'.format(char_format)),
+    (r'(\d+[.,]?\d+)\s?([€$\w]+)', r'\1{}\2'.format(CHAR_FORMAT)),
 ]
 
 
 def text_format(text):
-    for pattern, replacement in patterns:
+    for pattern, replacement in PATTERNS:
         text = re.sub(pattern, replacement, text)
     return text
