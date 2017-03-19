1ad9f40466f5b7665e9b978db06214b22b306dde
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-03-19 21:43:35 +0300

    extra variables for template

--- server.py
+++ server.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def form():
-    return render_template('form.html', text=('', ''))
+    return render_template('form.html')
 
 
 @app.route('/', methods=['POST'])
 def nice_format():
     input_text = request.form['text']
     result = text_format(input_text)
-    return render_template('form.html', text=(input_text, result))
+    return render_template('form.html', source_text=input_text, answer_text=result)
 
 if __name__ == "__main__":
     app.run()
